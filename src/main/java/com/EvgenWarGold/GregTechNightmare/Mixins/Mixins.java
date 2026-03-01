3aff728fee2fe147d4633bb7e521958e14a548a9
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2026-03-01 22:16:02 +0400

    Fix Tree Sprouter leaves crush

---
+++ src/main/java/com/EvgenWarGold/GregTechNightmare/Mixins/Mixins.java
@@ -0,0 +1,28 @@
+package com.EvgenWarGold.GregTechNightmare.Mixins;
+
+import javax.annotation.Nonnull;
+
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder;
+
+public enum Mixins implements IMixins {
+
+    MINECRAFT_LATE(new MixinBuilder("Minecraft Late").addClientMixins("FixTreeSprouterCrushMixins")
+        .setPhase(Phase.LATE)),
+
+    // MINECRAFT_EARLY(new MixinBuilder("Minecraft Early").addClientMixins("Test", "Test")
+    // .setPhase(Phase.EARLY))
+    ;
+
+    private final MixinBuilder builder;
+
+    Mixins(MixinBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Nonnull
+    @Override
+    public MixinBuilder getBuilder() {
+        return builder;
+    }
+}
