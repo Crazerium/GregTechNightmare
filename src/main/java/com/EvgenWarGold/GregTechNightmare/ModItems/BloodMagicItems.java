e821603979238479794ea35a05dfe8bf3eaef01e
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2026-03-10 19:01:48 +0400

    Add recipe for Laser Meteor Miner

--- src/main/java/com/EvgenWarGold/GregTechNightmare/ModItems/BloodMagicItems.java
+++ src/main/java/com/EvgenWarGold/GregTechNightmare/ModItems/BloodMagicItems.java
@@ -8,10 +8,14 @@ import gregtech.api.enums.Mods;
 public class BloodMagicItems extends ModHandler {
 
     public final ModItem BoundPickaxe;
+    public final ModItem LifeShard;
+    public final ModItem SoulShard;
 
     public BloodMagicItems() {
         super(Mods.BloodMagic);
 
         BoundPickaxe = new ModItem(mod, "boundPickaxe", 0, "Bound Pickaxe");
+        LifeShard = new ModItem(mod, "bloodMagicBaseItems", 28, "Life Shard");
+        SoulShard = new ModItem(mod, "bloodMagicBaseItems", 29, "Soul Shard");
     }
 }
