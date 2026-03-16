ca9285891de45687b0b6aee91e07342c5b148c38
Crazerium <artemlistopad21361@mail.ru>
2026-03-17 02:30:51 +1000

    Fix Tooltip BloodEnchanter

--- src/main/java/com/EvgenWarGold/GregTechNightmare/GregTech/MultiBlock/Processing/IV/GTN_BloodEnchanter.java
+++ src/main/java/com/EvgenWarGold/GregTechNightmare/GregTech/MultiBlock/Processing/IV/GTN_BloodEnchanter.java
@@ -579,7 +579,7 @@ public class GTN_BloodEnchanter extends GTN_MultiBlockBase<GTN_BloodEnchanter> {
         String[] ret = new String[origin.length + 2];
         System.arraycopy(origin, 0, ret, 0, origin.length);
         ret[origin.length] = EnumChatFormatting.AQUA + "Mode: " + EnumChatFormatting.GOLD + modeName;
-        ret[origin.length + 1] = EnumChatFormatting.AQUA + "LP Required: " + EnumChatFormatting.GOLD + mLastNeedLP;
+        ret[origin.length + 1] = EnumChatFormatting.AQUA + "Blood Required: " + EnumChatFormatting.GOLD + mLastNeedLP;
         return ret;
     }
 
