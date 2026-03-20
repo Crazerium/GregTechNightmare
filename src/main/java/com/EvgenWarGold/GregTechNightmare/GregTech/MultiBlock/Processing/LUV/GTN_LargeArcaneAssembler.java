402b951939a6250e40b5d20dcc798630e44e87a4
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2026-03-20 15:56:16 +0400

    Replace Large Arcane Assembler

--- src/main/java/com/EvgenWarGold/GregTechNightmare/GregTech/MultiBlock/Processing/LUV/GTN_LargeArcaneAssembler.java
+++ src/main/java/com/EvgenWarGold/GregTechNightmare/GregTech/MultiBlock/Processing/LUV/GTN_LargeArcaneAssembler.java
@@ -1,33 +1,21 @@
 package com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.Processing.LUV;
 
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.*;
-import static gregtech.api.enums.HatchElement.*;
-import static gregtech.api.util.GTStructureUtility.buildHatchAdder;
-import static thaumcraft.common.config.ConfigBlocks.blockMetalDevice;
-import static thaumcraft.common.config.ConfigBlocks.blockStoneDevice;
-
-import java.util.ArrayList;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.world.World;
-
-import org.jetbrains.annotations.NotNull;
-
 import com.EvgenWarGold.GregTechNightmare.GregTech.Api.ArcaneAssemblerCraftingFX;
+import com.EvgenWarGold.GregTechNightmare.GregTech.Api.MultiblockArea;
+import com.EvgenWarGold.GregTechNightmare.GregTech.Api.MultiblockOffsets;
 import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.MultiBlockClasses.GTN_Casings;
-import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.MultiBlockClasses.GTN_MultiBlockBase;
 import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.MultiBlockClasses.GTN_ProcessingLogic;
+import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.MultiBlockClasses.GTN_StructureUtility;
 import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.MultiBlockClasses.OverclockType;
+import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.NewMultiBlockClasses.ElementBuilder;
+import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.NewMultiBlockClasses.GTN_MultiBlockTooltipBuilder;
+import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.NewMultiBlockClasses.GTN_NewMultiBlockBase;
+import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.NewMultiBlockClasses.StructureVariant;
+import com.EvgenWarGold.GregTechNightmare.GregTech.MultiBlock.NewMultiBlockClasses.TierData;
 import com.EvgenWarGold.GregTechNightmare.GregTech.Recipe.GTN_Recipe;
 import com.EvgenWarGold.GregTechNightmare.GregTech.Recipe.MetaData.SimpleMetaData;
-import com.EvgenWarGold.GregTechNightmare.Utils.Constants;
+import com.EvgenWarGold.GregTechNightmare.Utils.Authors;
 import com.gtnewhorizon.structurelib.structure.IStructureDefinition;
-
-import bartworks.API.BorosilicateGlass;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
@@ -38,7 +26,12 @@ import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.CheckRecipeResultRegistry;
 import gregtech.api.recipe.check.SimpleCheckRecipeResult;
 import gregtech.api.util.GTRecipe;
-import gregtech.api.util.MultiblockTooltipBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.research.ResearchCategories;
@@ -47,9 +40,21 @@ import thaumcraft.api.research.ResearchItem;
 import thaumcraft.api.visnet.VisNetHandler;
 import thaumcraft.common.lib.research.ResearchManager;
 
-public class GTN_LargeArcaneAssembler extends GTN_MultiBlockBase<GTN_LargeArcaneAssembler> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    protected ArrayList<String> Research = new ArrayList<>();
+import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlock;
+import static gregtech.api.enums.HatchElement.Energy;
+import static gregtech.api.enums.HatchElement.InputBus;
+import static gregtech.api.enums.HatchElement.OutputBus;
+import static thaumcraft.common.config.ConfigBlocks.blockMetalDevice;
+import static thaumcraft.common.config.ConfigBlocks.blockStoneDevice;
+
+public class GTN_LargeArcaneAssembler extends GTN_NewMultiBlockBase<GTN_LargeArcaneAssembler> {
+
+    TierData glass = createTierData("glass");
+    protected ArrayList<String> research = new ArrayList<>();
 
     public GTN_LargeArcaneAssembler(int id, String name) {
         super(id, name);
@@ -59,51 +64,68 @@ public class GTN_LargeArcaneAssembler extends GTN_MultiBlockBase<GTN_LargeArcane
         super(name);
     }
 
-    public int getOffsetHorizontal() {
-        return 2;
+    @Override
+    public List<StructureVariant<GTN_LargeArcaneAssembler>> getStructureVariants() {
+        return Arrays.asList(
+            new StructureVariant<>(
+                "AdvancedBBF",
+                // spotless:off
+                new String[][]{
+                    {"BBBBB","BAAAB","BAAAB","BAAAB","BBBBB"},
+                    {"BAAAB","A   A","A   A","A   A","BAAAB"},
+                    {"BAAAB","A   A","A E A","A   A","BAAAB"},
+                    {"BAAAB","A   A","A   A","A   A","BAAAB"},
+                    {"BAAAB","A   A","A D A","A   A","BAAAB"},
+                    {"BB~BB","BCCCB","BCCCB","BCCCB","BBBBB"}
+                },
+                //spotless:on
+                new MultiblockOffsets(2, 5, 0),
+                new MultiblockArea(5, 6, 5),
+                1,
+                GTN_Casings.MagicCasing));
     }
 
-    public int getOffsetVertical() {
-        return 5;
-    }
-
-    public int getOffsetDepth() {
-        return 0;
-    }
-
-    public GTN_Casings getMainCasings() {
-        return GTN_Casings.MagicCasing;
-    }
-
+    @Override
     public GTN_LargeArcaneAssembler createNewMetaEntity() {
         return new GTN_LargeArcaneAssembler(this.mName);
+    }
+
+    @Override
+    public void createGtnTooltip(GTN_MultiBlockTooltipBuilder builder) {
+        builder.addInputBus()
+            .addOutputBus()
+            .addEnergyHatch();
+    }
+
+    @Override
+    public Authors getAuthor() {
+        return Authors.CRAZER;
+    }
+
+    @Override
+    public IStructureDefinition<GTN_LargeArcaneAssembler> getStructureDefinition() {
+        return buildStructureDefinition(
+            builder -> builder.addElement('C', ofBlock(blockMetalDevice, 9))
+                .addElement('A', GTN_StructureUtility.createAllTieredGlass(glass))
+                .addElement('D', ofBlock(blockStoneDevice, 10))
+                .addElement('E', ofBlock(blockStoneDevice, 11))
+                .addElement(
+                    'B',
+                    ElementBuilder.create(GTN_LargeArcaneAssembler.class, this)
+                        .casing(mainCasing)
+                        .hatches(InputBus, OutputBus, Energy)
+                        .build()));
     }
 
     public boolean isNoMaintenanceIssue() {
         return true;
     }
 
-    public String[][] getShape() {
-        // spotless:off
-        return new String[][]{
-            {"BBBBB","BAAAB","BAAAB","BAAAB","BBBBB"},
-            {"BAAAB","A   A","A   A","A   A","BAAAB"},
-            {"BAAAB","A   A","A E A","A   A","BAAAB"},
-            {"BAAAB","A   A","A   A","A   A","BAAAB"},
-            {"BAAAB","A   A","A D A","A   A","BAAAB"},
-            {"BB~BB","BCCCB","BCCCB","BCCCB","BBBBB"}
-        };
-        //spotless:on
-    }
-
-    private byte glassTier = 0;
-
     @Override
     public void saveNBTData(NBTTagCompound aNBT) {
         super.saveNBTData(aNBT);
-        aNBT.setByte("glassTier", glassTier);
         NBTTagList list = new NBTTagList();
-        for (String key : Research) {
+        for (String key : research) {
             NBTTagCompound tag = new NBTTagCompound();
             tag.setString("ResearchKey", key);
             list.appendTag(tag);
@@ -114,12 +136,11 @@ public class GTN_LargeArcaneAssembler extends GTN_MultiBlockBase<GTN_LargeArcane
     @Override
     public void loadNBTData(NBTTagCompound aNBT) {
         super.loadNBTData(aNBT);
-        glassTier = aNBT.getByte("glassTier");
-        Research.clear();
+        research.clear();
         if (aNBT.hasKey("ResearchList")) {
             NBTTagList list = aNBT.getTagList("ResearchList", 10);
             for (int i = 0; i < list.tagCount(); i++) {
-                Research.add(
+                research.add(
                     list.getCompoundTagAt(i)
                         .getString("ResearchKey"));
             }
@@ -135,14 +156,14 @@ public class GTN_LargeArcaneAssembler extends GTN_MultiBlockBase<GTN_LargeArcane
         String owner = aBaseMetaTileEntity.getOwnerName();
         if (owner == null || owner.isEmpty()) return;
 
-        Research.clear();
+        research.clear();
 
         for (ResearchCategoryList category : ResearchCategories.researchCategories.values()) {
 
             for (ResearchItem item : category.research.values()) {
 
                 if (ResearchManager.isResearchComplete(owner, item.key)) {
-                    Research.add(item.key);
+                    research.add(item.key);
                 }
             }
         }
@@ -169,58 +190,6 @@ public class GTN_LargeArcaneAssembler extends GTN_MultiBlockBase<GTN_LargeArcane
             double z = baseZ + (world.rand.nextDouble() - 0.5D) * 4.0D;
             Minecraft.getMinecraft().effectRenderer.addEffect(new ArcaneAssemblerCraftingFX(world, x, y, z));
         }
-    }
-
-    public void createGtnTooltip(MultiblockTooltipBuilder builder) {
-        builder.addInfo(this.tr("tooltip.00"))
-            .addInfo(this.tr("tooltip.01"))
-            .addInfo(this.tr("tooltip.02"))
-            .addInfo(this.tr("tooltip.03"))
-            .addInfo(this.tr("tooltip.04"))
-            .addInfo(Constants.AUTHOR_CRAZER)
-            .beginStructureBlock(5, 6, 5, false)
-            .addEnergyHatch(EnumChatFormatting.GOLD + "1", 1)
-            .addInputBus(EnumChatFormatting.GOLD + "1", 1)
-            .addOutputBus(EnumChatFormatting.GOLD + "1", 1);
-    }
-
-    @Override
-    public boolean GTN_checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
-        for (MTEHatchEnergy hatch : mEnergyHatches) {
-            if (hatch.mTier > glassTier) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public IStructureDefinition<GTN_LargeArcaneAssembler> getStructureDefinition() {
-        return IStructureDefinition.<GTN_LargeArcaneAssembler>builder()
-            .addShape(getStructurePieceMain(), transpose(getShape()))
-            .addElement('C', ofBlock(blockMetalDevice, 9))
-            .addElement(
-                'A',
-                withChannel(
-                    "glass",
-                    BorosilicateGlass.ofBoroGlass(
-                        (byte) 0,
-                        (byte) 1,
-                        Byte.MAX_VALUE,
-                        (te, t) -> te.glassTier = t,
-                        te -> te.glassTier)))
-            .addElement('D', ofBlock(blockStoneDevice, 10))
-            .addElement('E', ofBlock(blockStoneDevice, 11))
-            .addElement(
-                'B',
-                buildHatchAdder(GTN_LargeArcaneAssembler.class).atLeast(InputBus, OutputBus, Energy)
-                    .casingIndex(getMainCasings().textureId)
-                    .dot(2)
-                    .buildAndChain(
-                        onElementPass(
-                            GTN_LargeArcaneAssembler::mainCasingAdd,
-                            ofBlock(getMainCasings().getBlock(), getMainCasings().meta))))
-            .build();
     }
 
     @Override
@@ -272,4 +241,13 @@ public class GTN_LargeArcaneAssembler extends GTN_MultiBlockBase<GTN_LargeArcane
             .setMaxParallelSupplier(this::getMaxParallelRecipes);
     }
 
+    @Override
+    public boolean GTN_checkMachine(IGregTechTileEntity aBaseMetaTileEntity, ItemStack aStack) {
+        for (MTEHatchEnergy hatch : mEnergyHatches) {
+            if (hatch.mTier > glass.getCasingTier()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
